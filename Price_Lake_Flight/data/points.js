1370f824f2b3bf366cac806316fb32524574667e
keeferq <quinn.keefer@gmail.com>
2025-03-02 21:58:10 -0500

    Update points.js

--- Price_Lake_Flight/data/points.js
+++ Price_Lake_Flight/data/points.js
@@ -5,7 +5,7 @@ var point = {
         "properties": {
             "name": "Start Point",
             "description": "Where drone began collecting sonar data. The image below is from a previous flight but shows how the tethered sonar looks. The silver sensor sits just below the water line to collect sonar data.",
-            "image": "/Price_Lake_Flight/data/images/sonar_tether.jpg"
+            "image": "data/images/sonar_tether.jpg"
         },
         "geometry": {
             "coordinates": [-81.73175628096352, 36.13882671464289],
@@ -17,7 +17,7 @@ var point = {
         "properties": {
             "name": "End Point",
             "description": "End of the data collection. This flight ended with around 40% battery. Image is from a canoe which we used to collect ground control data ",
-            "image": "/Price_Lake_Flight/data/images/price_lake.jpg"
+            "image": "data/images/price_lake.jpg"
         },
         "geometry": {
             "coordinates": [-81.732176032518, 36.138732795048995],
@@ -40,7 +40,7 @@ var point = {
         "properties": {
             "name": "Drone Launch Area",
             "description": "The drone was launched in the Price Lake parking area. This was because there was no room near the lake to take off from. ",
-            "image": "/Price_Lake_Flight/data/images/launch_area.png"
+            "image": "data/images/launch_area.png"
         },
         "geometry": {
             "coordinates": [-81.73227820636605, 36.139122461284934],
