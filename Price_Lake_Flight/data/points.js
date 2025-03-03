69ed6f5a93029677b3434563a882471f5fedb2c9
keeferq <quinn.keefer@gmail.com>
2025-03-02 21:22:11 -0500

    Add files via upload

---
+++ Price_Lake_Flight/data/points.js
@@ -0,0 +1,51 @@
+var point = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "name": "Start Point",
+            "description": "Where drone began collecting sonar data. The image below is from a previous flight but shows how the tethered sonar looks. The silver sensor sits just below the water line to collect sonar data.",
+            "image": "/Price_Lake_Flight/data/images/sonar_tether.jpg"
+        },
+        "geometry": {
+            "coordinates": [-81.73175628096352, 36.13882671464289],
+            "type": "Point"
+        },
+        "id": 1
+    }, {
+        "type": "Feature",
+        "properties": {
+            "name": "End Point",
+            "description": "End of the data collection. This flight ended with around 40% battery. Image is from a canoe which we used to collect ground control data ",
+            "image": "/Price_Lake_Flight/data/images/price_lake.jpg"
+        },
+        "geometry": {
+            "coordinates": [-81.732176032518, 36.138732795048995],
+            "type": "Point"
+        },
+        "id": 2
+    }, {
+        "type": "Feature",
+        "properties": {
+            "name": "Observation Area",
+            "description": "Due to the launch site being behind a stand of trees, the drone was flown over them before we moved to get a clear view of the flight on the trail. "
+        },
+        "geometry": {
+            "coordinates": [-81.73174409262474, 36.138908581441214],
+            "type": "Point"
+        },
+        "id": 3
+    }, {
+        "type": "Feature",
+        "properties": {
+            "name": "Drone Launch Area",
+            "description": "The drone was launched in the Price Lake parking area. This was because there was no room near the lake to take off from. ",
+            "image": "/Price_Lake_Flight/data/images/launch_area.png"
+        },
+        "geometry": {
+            "coordinates": [-81.73227820636605, 36.139122461284934],
+            "type": "Point"
+        },
+        "id": 4
+    }]
+}
---
+++ Price_Lake_Flight/readme.md
@@ -0,0 +1,12 @@
+# Price Lake Drone Flight
+A web map of a drone flight conducted at Price Lake in North Carolina. 
+
+## Data
+The flight data, and photos were collected in the field by the author. While the basemap was provided by [USGS](https://usgs.gov/) and the orthophoto was downloaded from [NC One Map](https://www.nconemap.gov/)
+
+## Usage 
+This project is intended to showcase the flight.
+
+## Libraries 
+This map was created using the Leaflet library, with the coding being done in HTML and Javascript. 
+
